3898e8f452c0c088970112fcbd6b88fdbda3a78f
nikishkaa <nikita.goutor@gmail.com>
2024-02-14 19:02:26 +0300

    Update task Fibonacci number.

--- src/by/itsteap/goutor/javatask/stage11/task/fibonaccinumber/controller/Main.java
+++ src/by/itsteap/goutor/javatask/stage11/task/fibonaccinumber/controller/Main.java
@@ -1,6 +1,6 @@
 package by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.controller;
 
-import by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.model.FibonacciLogic;
+import by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.model.NumberLogic;
 import by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.util.FibonacciUI;
 import by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.util.NumberFibonacciInitializer;
 import by.itsteap.goutor.javatask.stage11.util.view.Printer;
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int number = NumberFibonacciInitializer.getFibonacciNumber();
 
-        int b = FibonacciLogic.getFibonacciNumber(number);
+        int b = NumberLogic.getFibonacciNumber(number);
 
 
         Printer.print(FibonacciUI.getFibonacciUI(b));
--- src/by/itsteap/goutor/javatask/stage11/task/fibonaccinumber/model/NumberLogic.java
+++ src/by/itsteap/goutor/javatask/stage11/task/fibonaccinumber/model/NumberLogic.java
@@ -1,6 +1,6 @@
 package by.itsteap.goutor.javatask.stage11.task.fibonaccinumber.model;
 
-public class FibonacciLogic {
+public class NumberLogic {
     public static final int FIRST_FIBONACCI = 0;
     public static final int SECOND_FIBONACCI = 1;
 
@@ -13,15 +13,11 @@ public class FibonacciLogic {
         int a = FIRST_FIBONACCI;
         int b = SECOND_FIBONACCI;
 
-        int count = 3;
 
-        while (count <= number) {
-
+        for (int i = 3; i <= number; i++) {
             int temp = a + b;
             a = b;
             b = temp;
-
-            count++;
         }
 
         return b;
