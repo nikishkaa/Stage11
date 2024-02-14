5c8e9b0c363fc3067fcd8d27f500f72e42d91905
nikishkaa <nikita.goutor@gmail.com>
2024-02-14 12:39:08 +0300

    Update task all divisor.

--- src/by/itsteap/goutor/javatask/stage11/task/alldivisor/util/AllDivisorsUI.java
+++ src/by/itsteap/goutor/javatask/stage11/task/alldivisor/util/AllDivisorsUI.java
@@ -1,6 +1,5 @@
 package by.itsteap.goutor.javatask.stage11.task.alldivisor.util;
 
-import java.util.Arrays;
 
 public class AllDivisorsUI {
     public static StringBuilder getAllDivisorsUI(StringBuilder BUILDER) {
